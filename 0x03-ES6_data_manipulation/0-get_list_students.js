68e05074fd4e52636fef8600059efdd0d0636069
johnahumanbeing <mseewak@gmail.com>
2024-04-29 22:33:51 +0300

    Refactor getListStudents function to use a more efficient data structure for storing student information and improve code readability.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,8 +6,8 @@
  */
 
 export default function getListStudents() {
-    const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-    const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
-    const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-    return Array.of(studentOne, studentTwo, studentThree);
-  }
+  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
+  const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return Array.of(studentOne, studentTwo, studentThree);
+}
