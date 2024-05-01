59a8ffbc5af64ae5b8dd0cd0f9c9344fddfbead6
johnahumanbeing <mseewak@gmail.com>
2024-05-01 21:03:02 +0300

    building a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,38 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index: string]: any;
+}
+
+interface Director extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+    return `${firstName[0]} ${lastName}`;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface{
+    constructor (public firstName: string, public lastName: string) {}
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
