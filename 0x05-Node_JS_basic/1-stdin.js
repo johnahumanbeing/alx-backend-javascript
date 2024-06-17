38705dc78d892d0b85d78c9c3a867ebc773969f1
johnahumanbeing <mseewak@gmail.com>
2024-06-17 22:02:08 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,24 @@
+/**
+ * A script that reads and processes user input using the `stdin` object.
+ * - promts the user to input name
+ *  - reads the input from the user
+ *  -displays the input back to the user
+ * - ends by displaying a message before closing
+ */
+
+const { stdin, stdout } = process;
+
+stdout.write('Welcome to Holberton School, what is your name?\n');
+
+stdin.setEncoding('utf8');
+stdin.on('readable', () => {
+  const name = stdin.read();
+  if (name) {
+    stdout.write(`Your name is: ${name}`);
+  }
+}
+);
+stdin.on('end', () => {
+  stdout.write('This important software is now closing\n');
+}
+);
