1b734a1f5d5bd60d71666f73b1bd46a8f3d2f938
johnahumanbeing <mseewak@gmail.com>
2024-06-17 21:54:30 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints a message to stdout
+ * @param {string} string - message to be displayed
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
